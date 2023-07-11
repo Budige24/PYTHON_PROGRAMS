425a2f8b4609a6ab34193625e7175cfb8168fdff
Budige24 <vamsi.budige@innojc.com>
2023-07-11 13:34:27 +0530

    12-07-23

---
+++ conditional_statements/if_else.py
@@ -0,0 +1,11 @@
+# -------------------if else----------------
+
+age = 24
+if age > 18:
+    print("you can vote")
+
+elif age == 18:
+    print("you can vote")
+
+else:
+    print("you cannot vote")
