533bdf80f4e9cdbb0ca927d1c3ece782dfe2ffe8
Budige24 <vamsi.budige@innojc.com>
2023-07-12 18:12:39 +0530

    12-07-23

---
+++ 9_tuples/tuples.py
@@ -0,0 +1,30 @@
+# -------------tuple (same like list with out methods)----------------------
+
+a = ("mango", "apple", "vamshikrishna", "sruthi", 4, 24)
+print(a)
+
+# ---------------we can perform min, max and sum can be performed-----------
+b = (1, 2, 3, 4, 5, 6, 7, 8)
+print(min(b))
+print(max(b))
+print(sum(b))
+print(len(b))
+
+# ------------------Concatenation (adding of two tuples)--------------------
+t1 = (1, 2, 3)
+t2 = (4, 5, 6)
+print(t1 + t2)
+
+# ------------repeating of tuple ---------------------------------------------
+name = ("Sruthi", "vamshikrishna")
+print(name * 4)
+
+# --------------iteration (arranging a data ina list)--------------------------
+fullName = ("Sruthi Vamshikrishna")
+for i in fullName:
+    print(i)
+
+# --------------Membership (to find the element present in data)----------------
+firstname = ("Sruthi")
+print("S" in firstname)
+print("V" in firstname)
