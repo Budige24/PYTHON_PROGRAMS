500401bc08f4a00cb5d7d4f5002e2041eb2e1a59
Budige24 <vamsi.budige@innojc.com>
2023-07-13 12:42:11 +0530

    13-07-23

--- 10_Dictionaries/Dictionaries.py
+++ 10_Dictionaries/Dictionaries.py
@@ -1,26 +1,26 @@
 # ---------------------Dictionaries (its an data type)-------------------
 student = {'name': "Sruthi", 'DOB': 24 - 10 - 2000, 'location': "karimnagar", }
-# print(student['name'])
+print(student['name'])
 
 # ------------------Get-----------------------------------
 a = {1: "vamshi", 2: "sandeep", 3: "manasa"}
-# print(a.get(1))
+print(a.get(1))
 
 # ------------------keys----------------------------------
-# print(a.keys())
+print(a.keys())
 
 # ------------------------values--------------------------
 b = {1: "sruthi", 2: "shiva", 3: "ranjith"}
-# print(b.values())
+print(b.values())
 
 # ----------------------items-----------------------------
 c = {1: "ramu", 2: "manasa", 3: "riyan", 4: "mikky"}
-# print(c.items())
+print(c.items())
 
 # ---------------------Update-----------------------------
 d = {1: "sravan", 2: "madusudhan",}
 d.update({3:"Ruchitha"})
-# print(d)
+print(d)
 
 for i,j in {1: "ramu", 2: "manasa", 3: "riyan", 4: "mikky"}.items():
     print(i,j)
