3b55089f14f298efc0f0bad69b0fcaab6e9ba53d
Budige24 <vamsi.budige@innojc.com>
2023-07-13 12:38:21 +0530

    13-07-23

--- 9_tuples/tuples.py
+++ 9_tuples/tuples.py
@@ -1,5 +1,4 @@
 # -------------tuple (same like list with out methods)----------------------
-
 a = ("mango", "apple", "vamshikrishna", "sruthi", 4, 24)
 print(a)
 
@@ -15,16 +14,16 @@ t1 = (1, 2, 3)
 t2 = (4, 5, 6)
 print(t1 + t2)
 
-# ------------repeating of tuple ---------------------------------------------
-name = ("Sruthi", "vamshikrishna")
+# ------------repeating of tuple --------------------------------------------
+name = ("Sruthi", "Vamshikrishna")
 print(name * 4)
 
-# --------------iteration (arranging a data ina list)--------------------------
-fullName = ("Sruthi Vamshikrishna")
+# --------------iteration (arranging a data ina list)------------------------
+fullName = ("Sruthi", "Vamshikrishna")
 for i in fullName:
     print(i)
 
-# --------------Membership (to find the element present in data)----------------
+# --------------Membership (to find the element present in data)-------------
 firstname = ("Sruthi")
 print("S" in firstname)
 print("M" in firstname)
