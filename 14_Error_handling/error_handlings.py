913fd7b8d7529eec5dad5d64fa3b9a568efa596c
Budige24 <vamsi.budige@innojc.com>
2023-07-19 17:43:54 +0530

    19-07-23

--- 14_Error_handling/error_handlings.py
+++ 14_Error_handling/error_handlings.py
@@ -8,3 +8,13 @@ else:
     print("no error")
 finally:
     print("always")
+
+# ------------------Error handling ------------------------
+
+c = "chintu"
+try:
+    print(c+22)
+except TypeError:
+    print("type error1")
+except ValueError:
+    print("value error2")
