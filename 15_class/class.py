9436382ce8d1dd70a0c64d3c6d863b83ee0d0e50
VamsiKrishna Budige <vamsi.budige@innojc.com>
2023-07-26 12:30:17 +0530

    26-07-23

---
+++ 15_class/class.py
@@ -0,0 +1,18 @@
+# --------------class (blue print of an object)-------------
+
+# -------normal class syntax-----------
+class Classname():
+    a = 24
+    print(a)
+
+
+# ---------class method ---------------
+class student():  # class
+    sandeep = 29
+
+    def output(self):  # object
+        print(self.sandeep)
+
+
+b = student()  # object
+b.output()  # object.method
